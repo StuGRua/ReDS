7459fb76437e2175bc7f9f88534badc38b0e01df
Stu6 <Stu6@users.noreply.github.com>
2020-12-09 18:07:33 +0800

    1209_upd1

---
+++ 上机作业/宗老师/哈夫曼树/huffman.cpp
@@ -0,0 +1,178 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std;
+
+//哈夫曼树的存储结构
+typedef struct
+{
+	char data;   //存储数据
+	int weight;  //结点的权重
+	string huffman_code;  //存放哈夫曼码
+	int parent, lchild, rchild;  //结点的双亲、左孩子、右孩子的下标
+} HTNode, * HuffmanTree;
+
+//两个最小结点
+typedef struct
+{
+	int s1;
+	int s2;
+} MIN;
+
+//选择结点权值最小的两个结点
+MIN Select(HuffmanTree HT, int n)
+{
+	int min, secmin, s1, s2;
+	min = 10000;
+	secmin = 10000;
+	MIN code;
+	s1 = 1;
+	s2 = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		if (HT[i].parent == 0 && (HT[i].weight < min))
+		{
+			min = HT[i].weight;
+			s1 = i;
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (HT[i].parent == 0 && (HT[i].weight < secmin) && (i != s1))
+		{
+			secmin = HT[i].weight;
+			s2 = i;
+		}
+	}
+	code.s1 = s1;
+	code.s2 = s2;
+	return code;
+
+}
+
+//将哈夫曼码存储在结构体num中
+void putlorinnum(HuffmanTree& hft, int num)
+{
+	for (int i = num; i >= 1; i--)
+	{
+		if (hft[hft[i].parent].parent)
+		{
+			hft[i].huffman_code = hft[hft[i].parent].huffman_code + hft[i].huffman_code;
+		}
+	}
+}
+
+
+//创造哈夫曼树
+void CreateHuffmanTree(HuffmanTree& HT, int num)
+{
+
+	int m;
+	m = 2 * num - 1;
+	HT = new HTNode[m + 1];   //分配空间
+	for (int i = 1; i <= m; i++)   //初始化
+	{
+		HT[i].parent = 0;
+		HT[i].lchild = 0;
+		HT[i].rchild = 0;
+	}
+	cout << "请输入每个数据及其权值：" << endl;
+	for (int i = 1; i <= num; i++)
+	{
+		cin >> HT[i].data;
+		cin >> HT[i].weight;
+
+	}
+
+	for (int i = num + 1; i <= m; i++)  //构建哈夫曼树
+	{
+		MIN min;
+		min = Select(HT, i - 1);      //选择二叉树
+		HT[min.s1].parent = i;
+		HT[min.s2].parent = i;
+		HT[i].lchild = min.s1;
+		HT[min.s1].huffman_code = "0";
+		HT[i].rchild = min.s2;
+		HT[min.s2].huffman_code = "1";
+		HT[i].weight = HT[min.s1].weight + HT[min.s2].weight;
+		HT[i].data = -1;
+	}
+	putlorinnum(HT, m);
+	for (int i = 1; i <= m; i++)  //进行每个字符哈夫曼码的输出
+	{
+		if (HT[i].data != -1)
+		{
+			cout << HT[i].data << " 权重为" << HT[i].weight << "  ，哈夫曼码为：" << HT[i].huffman_code << endl;
+
+			cout << endl;
+		}
+	}
+}
+
+//将一串字符编译成哈夫曼码
+string changchartohft(HuffmanTree hft, string s, int m)
+{
+	string estring;
+	for (int i = 0; i <= s.size(); i++)
+	{
+		for (int x = 1; x <= m; x++)
+		{
+			if (hft[x].data == s[i])//查找哈夫曼树中相应的字符
+			{
+				estring = estring + hft[x].huffman_code;//哈夫曼码连接
+				x = m;
+			}
+		}
+	}
+	cout << estring << endl;
+	return estring;
+}
+
+
+
+//将一串哈夫曼码解译成一串字符
+void changhfttochar(HuffmanTree hft, string s, int m)
+{
+	string estring;
+	int pos = 0, first = 0;
+	for (int x = 0; x <= s.size(); x++)
+	{
+		pos++;
+		for (int i = 1; i <= m; i++)
+		{
+			if (hft[i].huffman_code == s.substr(first, pos))//截取字符串和哈夫曼中哈夫曼码对比
+			{
+				cout << hft[i].data;
+				first = pos + first;
+				pos = 0;
+			}
+		}
+	}
+	cout << endl;
+}
+
+int main()
+{
+	//ADBDCDADBADBBCBD
+	/*
+	测试数据：
+	4
+	A 3 B 5 C 2 D 6
+	A 19 B 31 C 13 D 38
+	*/
+	int num;  //结点的个数
+	string s1 = "ADBDCDADBADBBCBD", s2;
+	cout << "哈夫曼树叶子结点的个数：";
+	cin >> num;
+	//创造哈夫曼树
+	HuffmanTree HT;
+	CreateHuffmanTree(HT, num);
+
+
+	cout <<"对 " <<s1<<" 进行编码：" << endl;
+	s2 = changchartohft(HT, s1, num);
+
+	cout << "解码：" << endl;
+	changhfttochar(HT, s2, num);
+	return 0;
+}
---
+++ 上机作业/宗老师/图/graph.h
@@ -0,0 +1,197 @@
+#pragma once
+#include <iostream>
+#include "Edge.h"
+#include "queue"
+#include "stack"
+#define INFINITY INT_MAX
+#define VISITED 1
+#define UNVISITED 0
+using namespace std;
+template <class EdgeType>
+class Graph //图类型
+{
+public:
+	int vertexNum;  //图的顶点数目
+	int edgeNum;	//图的边数目
+	int *Mark;		//标记某顶点是否被访问过,访问过为1，没访问为0
+	bool Cflag; //环标记
+	queue<int> Path;
+	Graph(int verticesNum)
+	{
+		vertexNum = verticesNum;
+		edgeNum = 0;
+		Cflag=0;
+		Mark = new int[verticesNum];
+		for (int i = 0; i < verticesNum; i++)
+		{
+			Mark[i] = UNVISITED;
+		}
+	}
+	~Graph()
+	{
+		delete[] Mark;
+	}
+	virtual Edge<EdgeType> FirstEdge(int onevertex) = 0;		 //返回与顶点相关联的第一条边
+	virtual Edge<EdgeType> NextEdge(Edge<EdgeType> oneEdge) = 0; //返回与oneedge有相同起点的下一条边
+	int VerticesNum()											 //返回图的顶点个数
+	{
+		return vertexNum;
+	}
+	int edgesnum() //返回边个数
+	{
+		return edgeNum;
+	}
+	bool Isedge(Edge<EdgeType> oneEdge) //判断是否为边
+	{
+		if (oneEdge.weight > 0 && oneEdge.weight < INFINITY && oneEdge.end >= 0)
+			return true;
+		else
+			return false;
+	}
+	int StartVertex(Edge<EdgeType> oneEdge) //返回边的始点
+	{
+		return oneEdge.start;
+	}
+	int EndVertex(Edge<EdgeType> oneEdge) //返回边的终点
+	{
+		return oneEdge.end;
+	}
+	EdgeType Weight(Edge<EdgeType> oneEdge) //返回边的权重
+	{
+		return oneEdge.weight;
+	}
+	virtual void setEdge(int start, int end, EdgeType weight) = 0; //设置边
+	virtual void delEdge(int start, int end) = 0;				   //删除边
+	virtual void editEdge(int start, int end,EdgeType weight) = 0;
+	void visit(int v)											   //访问结点
+	{
+		cout << v << ' ';
+	}
+	void DFS(int v) //从某顶点开始递归深度优先搜索
+	{
+		//visit(v);
+		Mark[v] = VISITED;
+		for (Edge<EdgeType> e = FirstEdge(v); Isedge(e); e = NextEdge(e))//从未访问结点继续调用深度搜索，否则不调用
+		{
+			if (Mark[EndVertex(e)] == UNVISITED)
+				DFS(EndVertex(e));
+		}
+	}
+	void DFStraverse() //图的递归深度优先搜索
+	{
+		int i;
+		for (i = 0; i < VerticesNum(); i++)
+			Mark[i] = UNVISITED;
+		for (i = 0; i < VerticesNum(); i++)
+		{
+			if (Mark[i] == UNVISITED)
+				DFS(i);
+		}
+	}
+	void DFSNoReverse() //非递归法深度优先遍历，类似二叉树中序遍历
+	{
+		int i, v, u;//v为出发结点，u邻接点
+		stack<int> s;
+		for (i = 0; i < VerticesNum(); i++)
+			Mark[i] = UNVISITED;
+		for (i = 0; i < VerticesNum(); i++)
+		{
+			if (Mark[i] == UNVISITED)//未访问结点访问并入栈
+			{
+				s.push(i);
+				visit(i);
+				Mark[i] = VISITED;
+				while (!s.empty())//直到栈空为止
+				{
+					v = s.top();
+					Edge<EdgeType> e;
+					for (e = FirstEdge(v); Isedge(e); e = NextEdge(e))//未访问的点访问并入栈
+					{
+						u = EndVertex(e);
+						if (Mark[u] == UNVISITED)
+						{
+							s.push(u);
+							visit(u);
+							Mark[u] = VISITED;
+							break;
+						}
+					}
+					if (!Isedge(e))
+						s.pop();
+				}
+			}
+		}
+	}
+	void BFS(int v) //从某结点开始广度优先搜索
+	{
+		queue<int> Q;
+		// 标记入队
+		Mark[v] = VISITED;
+		visit(v);
+		Q.push(v);
+		while (!Q.empty())//执行到队中无元素
+		{
+			v = Q.front();
+			Q.pop();
+			for (Edge<EdgeType> e = FirstEdge(v); Isedge(e); e = NextEdge(e))//相邻的未访问点访问并入队
+			{
+				int u = Mark[EndVertex(e)];
+				int m = EndVertex(e);
+				if (u == UNVISITED)
+				{
+					visit(m);
+					Mark[m] = VISITED;
+					Q.push(m);
+				}
+			}
+		}
+	}
+	void BFStraverse() //图的广度优先搜索
+	{
+		int v;
+		for (v = 0; v < VerticesNum(); v++)
+			Mark[v] = UNVISITED;//归位
+		for (v = 0; v < VerticesNum(); v++)//从未访问的点开始搜索
+		{
+			if (Mark[v] == UNVISITED)
+				BFS(v);
+		}
+	}
+
+	void DFSI(int v)//深度搜索查找环，与常规DFS不同的是Mark的取值有三种，新增的值2代表正在处理中
+	{
+		if (Cflag)//Cflag为标记环位
+			return;
+		visit(v);
+		Mark[v] = 2;//标记处理中的状态
+		for (Edge<EdgeType> e = FirstEdge(v); Isedge(e); e = NextEdge(e))
+		{
+			if (Mark[EndVertex(e)] == UNVISITED)
+			{
+				
+				DFSI(EndVertex(e));
+			}
+			else if (Mark[EndVertex(e)] == 2)//如果路径返回到正在处理的路径点，即为找到环，标记并退出
+			{
+				Cflag = 1;
+				//cout << "Have cycle!"<<endl;
+				return;
+			}
+		}
+		Mark[v] = VISITED;//退出处理状态，标记已处理状态
+	}
+
+	bool DCycle()//显示环辅助函数
+	{
+		if(Cflag==1) 
+		{
+			cout << "There is a cycle."<<endl;
+			return 1;
+		}
+		else 
+		{
+			cout << "There is no cycle."<<endl;
+			return 0;
+		}
+	}
+};
